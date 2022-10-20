ad0692c64b10d1114a099bc724d8350b31e4d319
MrDiipo <68384682+MrDiipo@users.noreply.github.com>
2022-10-20 09:20:37 +0100

    Add bootloader instructions to output "Hello World"

--- boot.asm
+++ boot.asm
@@ -1,14 +1,32 @@
 ORG 0x7c00
 BITS 16
 
-start:
-        mov ah, 0eh
-        mov al, 'A'
-        mov bx, 0
-        int 0x10
+; command to run nasm -f bin ./boot.asm -o  ./boot.bin
+; qemu-system-x86_64 -hda ./boot.bin
 
+start:
+        mov si, message
+        call print
         jmp $
 
+print:
+        mov bx, 0
+.loop:
+        lodsb
+        cmp al, 0
+        je .done
+        call print_char
+        jmp .loop
+
+ .done:
+        ret
+
+print_char:
+        mov ah, 0eh
+        int 0x10
+        ret
+
+message:  db 'Hello World!', 0
 
 times 510-($ - $$) db 0
 dw 0xAA55
