884c7b96a43169ac62209fc4d1565452ef186e48
MrDiipo <68384682+MrDiipo@users.noreply.github.com>
2022-10-21 09:27:12 +0100

    Add BIOS parameter block

--- boot.asm
+++ boot.asm
@@ -1,11 +1,18 @@
 ORG 0
 BITS 16
 
+_start:
+        jmp short start
+        nop
+
+    times 33 db 0
+
 ; command to run nasm -f bin ./boot.asm -o  ./boot.bin
 ; qemu-system-x86_64 -hda ./boot.bin
 
-jmp 0x7c0: start
 start:
+        jmp 0x7c0: step2
+step2:
         cli ; Clear interrupts
         mov ax, 0x7c0
         mov ds, ax
