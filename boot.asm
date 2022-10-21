531f199be86a00f559ab2d7f29e0340f9f04b5d7
MrDiipo <68384682+MrDiipo@users.noreply.github.com>
2022-10-21 12:15:58 +0100

    Change to protected mode development

--- boot.asm
+++ boot.asm
@@ -1,5 +1,9 @@
-ORG 0
+ORG 0x7c00
 BITS 16
+
+CODE_SEG equ gdt_code - gdt_start
+DATA_SEG equ gdt_data - gdt_start
+
 
 _start:
         jmp short start
@@ -11,59 +15,75 @@ _start:
 ; qemu-system-x86_64 -hda ./boot.bin
 
 start:
-        jmp 0x7c0: step2
+        jmp 0 : step2
 
 
 step2:
         cli ; Clear interrupts
-        mov ax, 0x7c0
+        mov ax, 0x00
         mov ds, ax
         mov es, ax
 
-        mov ax, 0x00
         mov ss, ax
         mov sp, 0x7c00
         sti ; Enable Interrupts
 
-        mov ah ; READ SECTION COMMAND
-        mov al, 1 ; ONE SECTION TO READ
-        mov ch, 0 ; Cylinder low eight bits
-        mov cl, 2 ; Read section two
-        mov dh, 0 ; Head number
-        mov bx, buffer
-        int 0x13
-        jc error
+.load_protected:
+        cli
+        ldgt[gdt_descriptor]
+        mov eax, cr0
+        or eax, 0x1
+        mov cr0, eax
+        jmp CODE_SEG : load32
 
-        mov si, buffer
 
-        call print
+; Global Descriptor Table (GDT)
+gdt_start:
 
+gdt_null:
+        dd 0x0
+        dd 0x0
+
+; offset 0x8
+; Using the DEFAULT values
+; Check osdev.org for more details
+gdt_code:        ; Code Segment (CS) should point to this
+        dw 0xffff ; Segment limit 0-15 bits
+        dw 0       ; Base first 0-15 bits
+        db 0        ; Base 16-23 bits
+        db 0x9a   ; Access byte
+        db 11001111b ; High 4 bit flags and low 4 bit flags
+        db 0
+
+
+; offset 0x10
+gdt_data:         ; For DS, ES, FS, GS
+        dw 0xffff ; Segment limit 0-15 bits
+        dw 0       ; Base first 0-15 bits
+        db 0        ; Base 16-23 bits
+        db 0x92   ; Access byte
+        db 11001111b ; High 4 bit flags and low 4 bit flags
+        db 0
+
+gdt_end:
+
+gdt_descriptor:
+        dw gdt_end - gdt_start - 1
+        dw gdt_start
+
+
+[BITS_32]
+load32:
+        mov ax, DATA_SEG
+        mov ds, ax
+        mov es, ax
+        mov fs, ax
+        mov gs, ax
+        mov ss, ax
+        mov ebp, 0x00200000
+        mov esp, ebp
         jmp $
-
-error:
-        mov s1, error_message
-
-
-print:
-        mov bx, 0
-.loop:
-        lodsb
-        cmp al, 0
-        je .done
-        call print_char
-        jmp .loop
-
- .done:
-        ret
-
-error_message: db 'Failed to load sector', 0
-print_char:
-        mov ah, 0eh
-        int 0x10
-        ret
 
 
 times 510-($ - $$) db 0
 dw 0xAA55
-
-buffer:
