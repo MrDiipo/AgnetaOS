1f8c57dd5c06235aa2f2dfbe77dab771af7d0202
MrDiipo <68384682+MrDiipo@users.noreply.github.com>
2022-10-21 10:10:59 +0100

    Add custom interrupts

--- boot.asm
+++ boot.asm
@@ -12,6 +12,22 @@ _start:
 
 start:
         jmp 0x7c0: step2
+
+handle_zero:
+        mov ah, 0eh
+        mov al, 'A'
+        mov bx, 0x00
+        int 0x10
+        iret
+
+handle_one:
+        mov ah, 0eh
+        mov al, 'V'
+        mov bx, 0x00
+        int 0x10
+        iret
+
+
 step2:
         cli ; Clear interrupts
         mov ax, 0x7c0
@@ -22,6 +38,14 @@ step2:
         mov ss, ax
         mov sp, 0x7c00
         sti ; Enable Interrupts
+
+        mov word[ss:0x00], handle_zero
+        mov word[ss:0x02], 0x7c0
+
+        mov word[ss:0x04], handle_one
+
+        int 0
+
         mov si, message
         call print
         jmp $
