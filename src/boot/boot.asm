b272e063710966d3c5b7cdf4f59c34047c4f45de
oladipupo <elegbedeola@gmail.com>
2022-10-23 22:42:49 +0100

    Add build scripts

--- src/boot/boot.asm
+++ src/boot/boot.asm
@@ -34,7 +34,8 @@ step2:
         mov eax, cr0
         or eax, 0x1
         mov cr0, eax
-        jmp CODE_SEG : load32
+       ; jmp CODE_SEG : load32
+       jmp $
 
 
 ; Global Descriptor Table (GDT)
@@ -71,23 +72,6 @@ gdt_descriptor:
         dw gdt_end - gdt_start - 1
         dw gdt_start
 
-
-[BITS 32]
-load32:
-        mov ax, DATA_SEG
-        mov ds, ax
-        mov es, ax
-        mov fs, ax
-        mov gs, ax
-        mov ss, ax
-        mov ebp, 0x00200000
-        mov esp, ebp
-
-        ; Enable the A20 line
-        in al, 0x92
-        or al, 2
-        out 0x92, al
-        jmp $
 
 
 times 510-($ - $$) db 0
