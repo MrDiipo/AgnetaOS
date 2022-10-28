63a1d2542ba0c9bf2a70e9a4beeaf3eb82cde01f
MrDiipo <68384682+MrDiipo@users.noreply.github.com>
2022-10-28 09:45:12 +0100

    Design heap interface

---
+++ src/memory/heap/heap.h
@@ -0,0 +1,26 @@
+
+#ifndef AGNETAOS_HEAP_H
+#define AGNETAOS_HEAP_H
+
+#include "../../config.h"
+#include <stdint.h>
+#include <stddef.h>
+
+#define HEAP_BLOCK_TABLE_ENTRY_TAKEN 0x01
+#define HEAP_BLOCK_TABLE_ENTRY_FREE 0x00
+
+#define HEAP_BLOCK_HAS_NEXT 0b10000000
+#define HEAP_BLOCK_IS_FREE 0b01000000
+
+typedef unsigned char HEAP_BLOCK_TABLE_ENTRY;
+
+struct heap_table{
+    HEAP_BLOCK_TABLE_ENTRY* entries;
+    size_t total;
+};
+
+struct heap {
+    struct heap_table* table;
+    void* saddr; // start address
+};
+#endif //AGNETAOS_HEAP_H
