060202a04253ac4d6d467f85e165f967e53d9057
MrDiipo <68384682+MrDiipo@users.noreply.github.com>
2022-10-28 15:32:41 +0100

    Complete 100MB heap memory initialization

--- src/memory/heap/heap.c
+++ src/memory/heap/heap.c
@@ -1,7 +1,20 @@
 #include "heap.h"
 #include "../../kernel.h"
 #include <stdbool.h>
+#include "../memory.h"
 #include "../../status.h"
+
+static int heap_validate_table(void* ptr, void* end, struct heap_table* table) {
+    int res = 0;
+    size_t table_size = (size_t) (end - ptr);
+    size_t total_blocks = table_size / AGNETAOS_HEAP_BLOCK_SIZE; // gives total number of blocks  on the heap
+
+    if (table->total != total_blocks) {
+        res = -EINVARG;
+        goto out;
+    }
+    out: return res;
+}
 
 static bool heap_validate_alignment(void* ptr) {
     return ((unsigned int) ptr % AGNETAOS_HEAP_BLOCK_SIZE) == 0;
@@ -14,6 +27,16 @@ int heap_create(struct heap* heap, void* ptr, void* end, struct heap_table* tabl
         res = -EINVARG;
         goto out;
     }
+    memset(heap, 0, sizeof(struct heap)); // initialize the heap size
+    heap->saddr = ptr;
+    heap->table = table;
 
+    res = heap_validate_table(ptr, end, table);
+    if (res < 0) {
+        goto out;
+    }
+
+    size_t table_size = sizeof(HEAP_BLOCK_TABLE_ENTRY) * table->total;
+    memset(table->entries, HEAP_BLOCK_TABLE_ENTRY_FREE, table_size);
     out: return res;
 }
