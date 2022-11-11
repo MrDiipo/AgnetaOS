49230697cb9582a2df116b33344d12ba60b2d22d
MrDiipo <68384682+MrDiipo@users.noreply.github.com>
2022-11-11 14:53:19 +0100

    Add virtual file system file status feature

--- src/fs/file.c
+++ src/fs/file.c
@@ -145,6 +145,17 @@ int fopen(const char *filename, const char *mode_string) {
         return res;
 }
 
+int fstat(int fd, struct file_stat* stat) {
+    int res = 0;
+    struct  file_descriptor*desc = file_get_descriptor(fd);
+    if (!desc) {
+        res = -EIO;
+        goto ou;
+    }
+    res = desc->filesystem->stat(desc->disk, desc->private, stat);
+    out: return res;
+}
+
 int fseek(int fd, int offset, FILE_SEEK_MODE whence) {
     int res = 0;
     struct file_descriptor* desc = file_get_descriptor(fd);
