35d30af0d05c3eda850e7456a55fc646279e63fb
MrDiipo <68384682+MrDiipo@users.noreply.github.com>
2022-11-11 17:05:34 +0100

    Modify bootloader

--- src/boot/boot.asm
+++ src/boot/boot.asm
@@ -9,6 +9,29 @@ _start:
         jmp short start
         nop
 
+; FAT16 Header
+OEMIdentifier               db 'AGNETAOS'
+BytesPerSector             dw 0x200
+SectorsPerCluster         db 0x80
+ReservedSectors           dw 200
+FATCopies                   db 0x02
+RootDirEntries             dw 0x40
+NumSectors                dw 0x00
+MediaType                  db 0xF8
+SectorsPerFat              dw 0x100
+SectorsPerTrack          dw 0x20
+NumberOfHeads         dw 0x40
+HiddenSectors            dd 0x00
+SectorsBig                  dd 0x773594
+
+; Extended BPB (Dos 4.0)
+DriveNumber            db 0x80
+WinNTBit                 db 0x00
+Signature                 db 0x29
+VolumeID                dd 0xD105
+VolumeIDString       db 'AGNETAOS BOO'
+SystemIDString        db 'FAT16'
+
     times 33 db 0
 
 ; command to run nasm -f bin ./boot.asm -o  ./boot.bin
