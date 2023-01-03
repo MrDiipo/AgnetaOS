3c9491733bdbf6f0bbb0a0156f1744bd5be4a278
MrDiipo <68384682+MrDiipo@users.noreply.github.com>
2023-01-03 16:40:52 +0100

    Fix bug in disk streamer feature

--- src/disk/streamer.c
+++ src/disk/streamer.c
@@ -3,6 +3,8 @@
 #include "../config.h"
 #include "../memory/heap/kheap.h"
 
+#include <stdbool.h>
+
 struct disk_stream* diskstreamer_new(int disk_id) {
     struct disk* disk = disk_get(disk_id);
     if (!disk) {
@@ -22,21 +24,26 @@ int diskstreamer_seek(struct disk_stream* stream, int pos) {
 int diskstreamer_read(struct disk_stream* stream, void* out, int total) {
     int sector = stream->pos / AGNETAOS_SECTOR_SIZE;
     int offset = stream->pos % AGNETAOS_SECTOR_SIZE;
+    int total_to_read = total;
+    bool overflow = (offset + total_to_read) >= AGNETAOS_SECTOR_SIZE;
+
     char buf[AGNETAOS_SECTOR_SIZE];
+    if (overflow) {
+        total_to_read -= (offset + total_to_read) - AGNETAOS_SECTOR_SIZE;
+    }
 
     int res = disk_read_block(stream->disk, sector, 1, buf);
     if (res < 0) {
         goto out;
     }
-    int total_to_read = total > AGNETAOS_SECTOR_SIZE ? AGNETAOS_SECTOR_SIZE : total;
     for (int i = 0; i < total_to_read; i++) {
         *(char*)out = buf[offset+1];
     }
 
     // adjust the stream
     stream->pos += total_to_read;
-    if (total > AGNETAOS_SECTOR_SIZE) {
-        res = diskstreamer_read(stream, out, total - AGNETAOS_SECTOR_SIZE);
+    if (overflow) {
+        res = diskstreamer_read(stream, out, total - total_to_read);
     }
     out: return res;
 }
