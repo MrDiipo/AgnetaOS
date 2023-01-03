de4415a9368417289bf77ea5894b1c13698af78b
MrDiipo <68384682+MrDiipo@users.noreply.github.com>
2023-01-03 16:12:59 +0100

    Add getting key from keyboard buffer in user land

--- programs/blank/blank.asm
+++ programs/blank/blank.asm
@@ -5,12 +5,20 @@ global _start
 
 _start:
 
+        call getkey
         push message
-        mov eax, 1 ; Command print
+        mov eax, 1
         int 0x80
         add esp, 4
 
         jmp $
+
+getkey:
+        mov eax, 2 ; command getkey
+        int 0x00
+        cmp eax, 0x00
+        je getkey
+        ret
 
 section .data
 message: db 'Hello world!', 0
--- src/isr80h/io.h
+++ src/isr80h/io.h
@@ -5,5 +5,5 @@
 struct interrupt_frame;
 
 void* isr80h_command1_print(struct interrupt_frame* frame);
-
+void* isr80h_command2_getkey(struct interrupt_frame* frame);
 #endif //AGNETAOS_IO_H
